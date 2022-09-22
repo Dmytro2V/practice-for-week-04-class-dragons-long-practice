4f9c30245b1473b34fbd15ef3fba1748817f7278
Dmytro2V <op2vin@gmail.com>
2022-09-22 23:39:55 +0300

    done

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,31 @@
 // Your code here
+Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+  constructor (name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;    
+  }
+
+  dontInviteThemOverForDinner() {
+    for (const evilDoing of this.evilDoings) {
+      console.log(`${this.name} will ${evilDoing}`); 
+    }
+  };
+
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+  };   
+
+};
+
+
+
+/**********************************************
+************************************************/
+try {
+  module.exports = EvilDragon;
+} catch {
+  module.exports = null;
+}
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,23 @@
 // Your code here
+Dragon = require('./dragon');
+
+class FriendlyDragon extends Dragon {
+  constructor (name, color, lifeGoals, friend) {
+    super(name, color);
+    this.lifeGoals = lifeGoals;
+    this.friend = friend;
+  }
+  hasLifeGoals() {
+    for (const lifeGoal of this.lifeGoals)
+    console.log (`${this.name} likes to ${lifeGoal}`);
+  }
+  helpsPeople() {
+    return `${this.name} helps their friend ${this.friend}`
+  }
+};
+
+try {
+  module.exports = FriendlyDragon;
+} catch {
+  module.exports = null;
+}
